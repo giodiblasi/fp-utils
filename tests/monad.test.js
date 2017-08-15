f35068593d1fef0dcbe546b0b1440851d9a8aef6
DESKTOP-OF37SSJ\Giovanni <gio.diblasi@gmail.com>
2017-08-15 15:48:48 +0200

    added chain monad test

--- tests/monad.test.js
+++ tests/monad.test.js
@@ -3,21 +3,33 @@ let assert = require('assert');
 let monads = require('../fp/monads');
 let Container = require('../fp/container');
 
-let wrappingFunction = (value) => Container.of(value+1);
+let Monad=monads.Monad;
+
+let wrappingFunction = (value) => Monad.of(value+1);
 
 describe('Monad tests', function () {
     it('should return flat value', function () {
-        let monad=monads.Monad.of(5);
+        let monad=Monad.of(5);
         let res=monad.bind(wrappingFunction);
         assert.equal(res.__value, 6);
-        assert.equal(res.constructor,Container);
+        assert.equal(res.constructor,Monad);
     });
 
     it('should return flat value using helper', function () {
-        let monad=monads.Monad.of(5);
+        let monad=Monad.of(5);
         let res=monads.bind(wrappingFunction)(monad);
         assert.equal(res.__value, 6);
-        assert.equal(res.constructor,Container);
+        assert.equal(res.constructor,Monad);
     });
+
+    it('test chain of binds', function () {
+        let monad = Monad.of(5);
+        var res = monad.bind(wrappingFunction).bind(wrappingFunction);
+        assert.equal(res.__value, 7);
+        assert.equal(res.constructor,Monad);
+    });
+
+
+
 });
 
